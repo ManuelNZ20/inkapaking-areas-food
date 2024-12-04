d4ddcc9f2378768342d9acf2d9741c70c2814b59
Manuel Walter Navarro Zeta <manuel08n@gmail.com>
2024-12-04 06:56:13 -0500

    env.dart from resend

--- lib/core/env/env.dart
+++ lib/core/env/env.dart
@@ -13,9 +13,17 @@ class Environment {
     return '.env.dev';
   }
 
+  // SUPABASE
   static String get urlBase =>
       dotenv.env['SUPABASE_URL'] ?? 'Not SUPABASE_URL especified';
 
   static String get anonKey =>
       dotenv.env['SUPABASE_ANON_KEY'] ?? 'Not SUPABASE_ANON_KEY especified';
+
+  // RESEND
+  static String get apiKeyResend =>
+      dotenv.env['API_KEY_RESEND'] ?? 'Not API_KEY_RESEND especified';
+
+  static String get emailSender =>
+      dotenv.env['EMAIL_SENDER'] ?? 'Not EMAIL_SENDER especified';
 }
