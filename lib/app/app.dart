62816dd56fa360f73c471a776919aee00a19e450
Manuel Walter Navarro Zeta <manuel08n@gmail.com>
2024-12-11 12:02:53 -0500

    modified imports

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -2,32 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../config/config.dart';
 import '../core/core.dart';
-import '../features/auth/presentation/providers/providers.dart';
-import '../features/rrhh/presentation/providers/providers.dart';
 
 class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    // Escuchar las notificaciones de la app
-    // authNotifierProvider
-    // final authNotifier = ref.watch(authNotifierProvider);
-
-    // ref.listen(
-    //   userNotifierProvider,
-    //   (previous, next) {
-    //     if (authNotifier.hasUser) {
-    //       if (next.isFirstLoad) {
-    //         next.checkNewRequestUser();
-    //       }
-    //       if (authNotifier.user!.typeUser.id == 5) {
-    //         next.checkNewRequestUser();
-    //       }
-    //     }
-    //   },
-    // );
-
     final appRouter = ref.watch(appRouterProvider);
 
     return MaterialApp.router(
--- lib/features/config/data/data.dart
+++ lib/features/config/data/data.dart
@@ -1,2 +1,6 @@
 // Models
 export 'models/user.module.dart';
+// Datasource
+export 'datasources/config_supabase_data_sources_impl.dart';
+// Repository
+export 'repositories/config_repository_impl.dart';
