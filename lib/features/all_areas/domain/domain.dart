51ca504a8d6a4948d7b3c6fba0c12fdb3a231c93
Manuel Walter Navarro Zeta <manuel08n@gmail.com>
2024-12-13 22:23:30 -0500

    implement file exports to the domain layer

--- lib/features/all_areas/domain/domain.dart
+++ lib/features/all_areas/domain/domain.dart
@@ -4,3 +4,6 @@ export 'entities/user.dart';
 export 'datasources/user_data_sources.dart';
 // Repositories
 export 'repositories/user_repository.dart';
+// use cases
+export 'use_cases/get_user_by_id.dart';
+export 'use_cases/get_users.dart';
