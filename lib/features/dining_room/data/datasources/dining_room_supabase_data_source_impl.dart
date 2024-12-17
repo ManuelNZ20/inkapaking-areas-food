0b5087057122d3019546e9db46cb35a094172f18
Manuel Walter Navarro Zeta <manuel08n@gmail.com>
2024-12-16 19:08:54 -0500

    implement file, and functions in datasources, repositories

---
+++ lib/features/dining_room/data/datasources/dining_room_supabase_data_source_impl.dart
@@ -0,0 +1,74 @@
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+import '../../domain/domain.dart';
+import '../data.dart';
+
+class DiningRoomSupabaseDataSourceImpl extends DiningRoomRemoteDataSource {
+  final SupabaseClient client;
+
+  DiningRoomSupabaseDataSourceImpl({required this.client});
+  @override
+  Future<SaucerModel>? createSaucer(
+    String nameSaucer,
+    String nameDrink,
+    int scheduleId,
+    String createdAt,
+  ) async {
+    final response = await client
+        .from('saucer')
+        .insert({
+          'name_saucer': nameSaucer,
+          'name_drink': nameDrink,
+          'schedule_id': scheduleId,
+          'created_at': createdAt,
+        })
+        .select('''*,schedule(*)''')
+        .limit(1)
+        .single();
+    return SaucerModel.fromJson(response);
+  }
+
+  @override
+  Future<bool>? deleteSaucer(
+    int saucerId,
+  ) async {
+    final response =
+        await client.from('saucer').delete().eq('id', saucerId).select();
+    return response.isNotEmpty;
+  }
+
+  @override
+  Future<List<SaucerModel>>? listSaucers() async {
+    final response = await client.from('saucer').select('''*,schedule(*)''');
+    return response.map((e) => SaucerModel.fromJson(e)).toList();
+  }
+
+  @override
+  Future<List<ScheduleModel>>? listSchedules() async {
+    final response = await client.from('schedule').select();
+    return response.map((e) => ScheduleModel.fromJson(e)).toList();
+  }
+
+  @override
+  Future<SaucerModel>? updateSaucer(
+    int saucerId,
+    String nameSaucer,
+    String nameDrink,
+    int scheduleId,
+    String updateAt,
+  ) async {
+    final response = await client
+        .from('saucer')
+        .update({
+          'name_saucer': nameSaucer,
+          'name_drink': nameDrink,
+          'schedule_id': scheduleId,
+          'update_at': updateAt,
+        })
+        .eq('id', saucerId)
+        .select('''*,schedule(*)''')
+        .limit(1)
+        .single();
+    return SaucerModel.fromJson(response);
+  }
+}
---
+++ lib/features/dining_room/data/repositories/dining_room_repository_impl.dart
@@ -0,0 +1,92 @@
+import 'package:dartz/dartz.dart';
+
+import '../../../../core/core.dart';
+import '../../domain/domain.dart';
+
+class DiningRoomRepositoryImpl extends DiningRoomRepository {
+  final DiningRoomRemoteDataSource remoteDataSource;
+  final NetworkInfo networkInfo;
+
+  DiningRoomRepositoryImpl({
+    required this.remoteDataSource,
+    required this.networkInfo,
+  });
+
+  Future<Either<Failure, T>> _handleNetworkRequest<T>(
+    Future<T> Function() request,
+  ) async {
+    if (await networkInfo.isConnected) {
+      try {
+        final result = await request();
+        return Right(result);
+      } on ServerException catch (e) {
+        return Left(ServerFailure(e.message));
+      } on GenericException catch (e) {
+        return Left(CustomFailure(e.toString()));
+      }
+    } else {
+      return Left(ServerFailure("No hay conexión a Internet"));
+    }
+  }
+
+  @override
+  Future<Either<Failure, Saucer>>? createSaucer(
+    String nameSaucer,
+    String nameDrink,
+    int scheduleId,
+    String createdAt,
+  ) async {
+    return _handleNetworkRequest(() async {
+      return await remoteDataSource.createSaucer(
+        nameSaucer,
+        nameDrink,
+        scheduleId,
+        createdAt,
+      )!;
+    });
+  }
+
+  @override
+  Future<Either<Failure, bool>> deleteSaucer(
+    int saucerId,
+  ) {
+    return _handleNetworkRequest(() async {
+      return await remoteDataSource.deleteSaucer(saucerId) ?? false;
+    });
+  }
+
+  @override
+  Future<Either<Failure, List<Saucer>>>? listSaucers() async {
+    return _handleNetworkRequest(() async {
+      return await remoteDataSource.listSaucers()!;
+    });
+  }
+
+  @override
+  Future<Either<Failure, List<Schedule>>>? listSchedules() async {
+    return _handleNetworkRequest(
+      () async {
+        return await remoteDataSource.listSchedules()!;
+      },
+    );
+  }
+
+  @override
+  Future<Either<Failure, Saucer>>? updateSaucer(
+    int saucerId,
+    String nameSaucer,
+    String nameDrink,
+    int scheduleId,
+    String updateAt,
+  ) async {
+    return _handleNetworkRequest(() async {
+      return await remoteDataSource.updateSaucer(
+        saucerId,
+        nameSaucer,
+        nameDrink,
+        scheduleId,
+        updateAt,
+      )!;
+    });
+  }
+}
